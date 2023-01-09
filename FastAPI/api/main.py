d3371075ca0cd291eb99c9d3ec65d291a54731f0
River <riveriswild@icloud.com>
2023-01-09 21:49:13 +0300

    add requirements

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -8,10 +8,6 @@ class Article(BaseModel):
 
 app = FastAPI()
 
-@app.get('/')
-async def Index():
-    return 'hello'
-
 @app.get('/articles/{id}')
 def get_artile(id:int):
     return{'article':{id}}
