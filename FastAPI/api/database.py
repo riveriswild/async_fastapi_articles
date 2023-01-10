1fdf222ebc2506f45a528d8ed74d398fd80a02c9
River <riveriswild@icloud.com>
2023-01-10 21:19:54 +0300

    Set up db

--- FastAPI/api/database.py
+++ FastAPI/api/database.py
@@ -4,3 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL = "mysql://root@localhost/fastapi"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
