f72d3ea198f2aa420c543ff5578a249cf6c4f559
River <riveriswild@icloud.com>
2023-01-14 20:52:27 +0300

    Add async retrieve view

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, status
 
 from .schemas import ArticleSchema, IdArticleSchema
 from .db import metadata, database, engine, Article
+from typing import List
 
 
 metadata.create_all(engine)
@@ -20,3 +21,8 @@ async def insert_article(article:ArticleSchema):
     query = Article.insert().values(title=article.title, description=article.description)
     last_record_id = await database.execute(query)
     return {**article.dict(), "id": last_record_id}
+
+@app.get('/articles/', response_model=List[IdArticleSchema])
+async def get_articles():
+    query = Article.select()
+    return await database.fetch_all(query=query)
