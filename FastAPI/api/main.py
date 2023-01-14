6b4c8c492bc0768db2063ef7bc4bfc942be40d9c
River <riveriswild@icloud.com>
2023-01-14 20:46:59 +0300

    Add async create view

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from .db import metadata, database, engine
+from fastapi import FastAPI, status
+
+from .schemas import ArticleSchema
+from .db import metadata, database, engine, Article
 
 
 metadata.create_all(engine)
@@ -12,3 +14,9 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+    
+@app.post('/articles/', status_code=status.HTTP_201_CREATED )
+async def insert_article(article:ArticleSchema):
+    query = Article.insert().values(title=article.title, description=article.description)
+    last_record_id = await database.execute(query)
+    return {**article.dict(), "id": last_record_id}
